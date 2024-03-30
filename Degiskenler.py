3af0a9a71d70dcf6ab1e1691b88fd7bd2fad3b5b
bakidnmz <144884483+bakidnmz@users.noreply.github.com>
2024-03-30 22:33:52 +0300

    Add files via upload

---
+++ Degiskenler.py
@@ -0,0 +1,30 @@
+x = 3  # integer
+print(x, type(x))
+y = 5  # integer
+print(y, type(y))
+z = "Baki"  # string
+print(z, type(z))
+t = "Dönmez"  # string
+print(t, type(t)) 
+k = 1.5  # float
+print(k, type(k))
+m = True  # boolean
+print(m, type(m))
+x, y = y, x  # değişken değiştirme
+print(x, y)
+print(z+t)  # iki string ifade işleme alınabilir
+# Matematiksel Operatörler (x=5,y=3)
+print(x+y)  # toplama x+y=5+3=8
+print(x-y)  # çıkarma x-y=5-3=2
+print(x*y)  # çarpma x*y=5*3=15
+print(x/y)  # bölme x/y=5/3=1,6667
+print(x//y)  # tambölme x//y=5//3=1
+print(x % y)  # mod alma x%y=5%3=2 (5'in 3'e bölümünden kalan)
+print(x**y)  # üs alma x**y=5**3=125
+
+a=0
+a=a+3  # a=a+3 ile a+=3 aynı ifadedir
+print(a)
+b=0
+b+=3
+print(b)  # aynı kurgu diğer matematiksel operatörler ile de yapılabilir
